7d068b9578cf813e56a6cc25fb637bdbbacf398c
Pygmalion69 <pygmalion@xs4all.nl>
2025-05-23 16:35:46 +0200

    Adjust to .exe

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,8 +54,7 @@ compose.desktop {
         mainClass = "MainKt"
         nativeDistributions {
             targetFormats(
-                TargetFormat.Dmg,
-                TargetFormat.Msi,
+                TargetFormat.Exe,
                 TargetFormat.Deb)
             packageName = "GpxPlayer"
             packageVersion = "1.0.0"
