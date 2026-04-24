a91a047f14db9c6921925fc26f2f6ad4904f28fc
Pygmalion69 <pygmalion@xs4all.nl>
2026-04-24 13:22:39 +0200

    Fix CI actions

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,16 +20,16 @@ repositories {
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion.set(JavaLanguageVersion.of(17))
     }
-    sourceCompatibility = JavaVersion.VERSION_18
-    targetCompatibility = JavaVersion.VERSION_18
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
     compilerOptions {
-        // Compose 1.6.10 packages with ProGuard 7.2.2, which can read up to Java 18 bytecode.
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_18)
+        // Compose 1.6.10 packages with ProGuard 7.2.2; keep bytecode compatible with JDK 17 in CI.
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
